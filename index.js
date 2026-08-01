1acc5b025672bd095375eef51f56370f75d6ffb5
ryan <ryan_shami@icloud.com>
2026-08-01 14:45:33 -0500

    made it accessable to happy face

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new Client({
 });
 
 // The High Command role allowed to use these mass DM commands
-const STAFF_ROLE_ID = '1533189561872679024'; 
+const STAFF_ROLE_ID = '1496722761450782860'; 
 
 // ==========================================
 // SLASH COMMANDS DEFINITIONS
